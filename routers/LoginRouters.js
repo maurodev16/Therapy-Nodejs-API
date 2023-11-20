046fa5915df713964310d82bc97dc8b7aa088c3c
maurodev16 <mauro.developer.br@gmail.com>
2023-11-20 22:10:25 +0100

    ´...

--- routers/LoginRouters.js
+++ routers/LoginRouters.js
@@ -22,7 +22,7 @@ router.post("/login", async (req, res) => {
     let user;
 
     // Check if Email is an email using regular expression
-    const isEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/(email);
+    const isEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 
     if (isEmail) {
       user = await User.findOne({ email: email });
