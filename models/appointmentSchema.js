1f62e985cab1dadfe8c1085cc96fdd4b665e449c
Mauro Rodrigues <mauro.developer.br@gmail.com>
2023-12-15 11:26:49 +0100

    ...

--- models/appointmentSchema.js
+++ models/appointmentSchema.js
@@ -19,7 +19,7 @@ const appointmentSchema = new mongoose.Schema(
       required: true,
     },
     service_type_obj: [
-      { type: mongoose.Schema.Types.ObjectId, ref: "ServiceType",  default: {}, },
+      { type: mongoose.Schema.Types.ObjectId, ref: "ServiceType",  default: null, },
     ],
     status: {
       type: String,
@@ -27,7 +27,7 @@ const appointmentSchema = new mongoose.Schema(
       default: "open",
     },
     canceled_by: {
-      type: mongoose.Schema.Types.ObjectId, ref: "User", default: {},  },
+      type: mongoose.Schema.Types.ObjectId, ref: "User", default: null,  },
   },
   {
     timestamps: true,
